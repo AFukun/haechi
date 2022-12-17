020e63c2643a08a0686858d9cc78bbeb6a2e9ef7
Ether <antfinancialyujian@gmail.com>
2022-12-17 18:35:53 -0500

    update build_executable script

--- scripts/EC2-test/build_EC2_executables.sh
+++ scripts/EC2-test/build_EC2_executables.sh
@@ -6,16 +6,16 @@ mkdir -p build
 
 go build -o build/ahlbc $ROOT/cmd/ahl/beacon
 go build -o build/ahlshard $ROOT/cmd/ahl/shard
-go build -o build/ahlclient $ROOT/cmd/latency-client/ahl
-go build -o build/ahlclient $ROOT/cmd/user-client/ahl
+go build -o build/ahllatency $ROOT/cmd/latency-client/ahl
+go build -o build/ahluser $ROOT/cmd/user-client/ahl
 
 go build -o build/byshard $ROOT/cmd/byshard/coordinator
-go build -o build/byshardclient $ROOT/cmd/latency-client/byshard
-go build -o build/byshardclient $ROOT/cmd/user-client/byshard
+go build -o build/byshardlatency $ROOT/cmd/latency-client/byshard
+go build -o build/bysharduser $ROOT/cmd/user-client/byshard
 
 go build -o build/haechibc $ROOT/cmd/haechi/beacon
 go build -o build/haechishard $ROOT/cmd/haechi/shard
-go build -o build/haechiclient $ROOT/cmd/latency-client/haechi
-go build -o build/haechiclient $ROOT/cmd/user-client/haechi
+go build -o build/haechilatency $ROOT/cmd/latency-client/haechi
+go build -o build/haechiuser $ROOT/cmd/user-client/haechi
 
 chmod +x build/*
